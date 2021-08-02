e962a211ee7b20860498abe275d5e1c78defc2ee
lhjeong60 <lhjeong60@naver.com>
2021-08-02 23:02:51 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // 여기의 MONGO_URI는 배포 클라우드 서비스에서 지정해주는(지정하는?) 이름과 똑같이 맞추면 되는듯
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose');
+const { User } = require('./models/User');
+const config = require('./config/key');
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+// application/json
+app.use(express.json());
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hjlee:admin12345@boilerplate.bevfa.mongodb.net/BoilerPlate?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
+app.post('/register', (req, res) => {
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면, 그것들을 데이터 베이스에 삽입!
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});

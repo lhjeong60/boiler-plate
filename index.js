9c18ee1c043a157e34aff15e98870b18fa5fefdc
lhjeong60 <lhjeong60@naver.com>
2021-08-02 21:32:40 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hjlee:admin12345@boilerplate.bevfa.mongodb.net/BoilerPlate?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true, //공백 없애주는 역할
+        unique: 1 //중복 안되게끔
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: { // 유효성?
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = {User};
